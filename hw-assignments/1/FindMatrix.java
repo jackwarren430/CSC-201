dfb0ef001ad87098b8e1b46d3b6b3ca77493cc42
jackwarren430 <90930942+jackwarren430@users.noreply.github.com>
2022-09-11 14:03:25 -0400

    fixed problems

--- hw-assignments/1/FindMatrix.java
+++ hw-assignments/1/FindMatrix.java
@@ -1,29 +1,23 @@
 public class FindMatrix {
 	public static void main(String[] args) {
 		int[][] matrix = {{10, 5, 3}, {8, 4, 2}, {7, 3, 1}};
-		System.out.println(MatrixFind(6, matrix));
+		int k = 6;
+		System.out.println("is " + k + " in the matrix? " + MatrixFind(k, matrix));
 	}
 
 	public static boolean MatrixFind(int k, int[][] matrix){
-		for (int i = 0; i < matrix.length; i++) {
-			if (matrix[i][0] >= k && matrix[i][matrix.length - 1] <= k){
-				int top = matrix.length - 1;
-				int bottom = 0;
-				while (top != bottom){
-					int mid = (top + bottom) / 2;
-					if (matrix[i][mid] == k){
-						return true;
-					} else if (matrix[i][mid] > k){
-						bottom = mid + 1;
-					} else {
-						top = mid - 1;
-					}
-				}
-				if (matrix[i][top] == k){
-					return true;
-				}
+		int row = matrix.length - 1;
+		int col = 0;
+		while(row >= 0 && col < matrix.length){
+			int element = matrix[row][col];
+			if(element == k){
+				return true;
+			} else if (k > element) {
+				row -= 1;
+			} else {
+				col += 1;
 			}
-		} 
+		}
 		return false;
 	}
 }
