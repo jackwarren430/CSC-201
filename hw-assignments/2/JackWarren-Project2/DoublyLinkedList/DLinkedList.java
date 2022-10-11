66d702251630b8a6ecd03be825d419b025a51a59
Jack <jackwarren430@gmail.com>
2022-10-10 22:45:46 -0400

    started doubly linked list

--- hw-assignments/2/JackWarren-Project2/DoublyLinkedList/DLinkedList.java
+++ hw-assignments/2/JackWarren-Project2/DoublyLinkedList/DLinkedList.java
@@ -2,7 +2,73 @@ public class DLinkedList implements MyList{
 
     // define any variables you want using the Encapsulation design principle.
 
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public DLinkedList(){
+        head = new Node(null);
+        tail = new Node(null);
+        head.right = tail;
+        tail.left = head;
+        size = 0;
+    }
+
+    public void append(Object item){
+        Node toAdd = new Node(item);
+        toAdd.left = tail.left;
+        toAdd.right = tail;
+        tail.left.right = toAdd;
+        tail.left = toAdd;
+    }
+
+    public void insert(int index, Object item){
+
+    }
+
+    public void clear(){
+
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean replace(int index, Object item){
+        return true;
+    }
+
+    public void remove(int index){
+
+    }
+
+    public Object get(int index){
+        return 1;
+    }
+
+    public String toString(){
+        Node curr = head;
+        String toReturn = "";
+        for (int i = 0; i <= size; i++){
+            toReturn += curr.right.data + ", ";
+        }
+        return toReturn;
+    }
+
     private class Node{
         //a private nested Node class that users cannot access.
+        private Object data;
+        private Node left;
+        private Node right;
+        
+        private Node(Object data){
+            this.data = data;
+            left = null;
+            right = null;
+        }
     }
 }
---
+++ hw-assignments/2/JackWarren-Project2/DoublyLinkedList/DListTester.java
@@ -0,0 +1,9 @@
+public class DListTester {
+	public static void main(String[] args) {
+		DLinkedList list = new DLinkedList();
+		list.append(1);
+		System.out.println(list);
+		list.append(2);
+		System.out.println(list);
+	}
+}
