247c941ff5da30c3cd56943577e5b28ceaae183a
Jack <jackwarren430@gmail.com>
2022-11-16 22:00:58 -0500

    started graphs

--- hw-assignments/3/JackWarren-Project3/GraphMatrix.java
+++ hw-assignments/3/JackWarren-Project3/GraphMatrix.java
@@ -1,3 +1,70 @@
+import java.util.*;
+
 public class GraphMatrix implements Graph{
-    //TODO.
+    
+    private int[][] matrix;
+    private int[][] visited;
+    private int nodes;
+    private int edges;
+
+    public GraphMatrix(){
+
+    }
+
+    public void init(int n){
+        matrix = new int[n][n];
+        visited = new int[n][n];
+        nodes = n;
+        edges = 0;
+    }
+
+    public int nodeCount(){
+        return nodes;
+    }
+
+    public int edgeCount(){
+        return edges;
+    }
+
+    public boolean hasEdge(int v, int w){
+        
+    }
+
+    public void addEdge(int v, int w, int wgt){
+
+    }
+
+    public void removeEdge(int v, int w){
+
+    }
+
+    public int getWeight(int v, int w){
+
+    }
+
+    public void setWeight(int v, int w, int wgt){
+
+    }
+
+    public ArrayList<Integer> neighbors(int v){
+
+    }
+
+    public void resetVisited(){
+
+    }
+
+    public ArrayList<Integer> BFS(){
+
+    }
+
+    public boolean hasPath(int v, int w){
+
+    }
+
+    public ArrayList<Integer> topologicalSort(){
+
+    }
+
+
 }
--- hw-assignments/3/JackWarren-Project3/HashTables.java
+++ hw-assignments/3/JackWarren-Project3/HashTables.java
@@ -4,25 +4,39 @@ import java.lang.Math;
 public class HashTables{
 
     // Problem 4.
+    
+    //this method adds everything in array to a hashset, then iterates back through the array
+    //and checks to see if the target - array[i] is in the hashset
+    //if it is, that means both numbers that add up to target are in the array
+    //and they get returned
     public static void findSumPair(int target, int[] array){
         HashSet<Integer> nums = new HashSet<Integer>();
+        //adding array to hashset
         for (int i : array){
             nums.add(i);
         }
+        //iterate back through array
         for (int i : array){
+            //checks to see if the hashset contains the needed value
             if (nums.contains(target - i)){
                 System.out.println("Pair found: " + i + "," + (target - i));
                 return;
             }
         }
+        //if no pairs are found, prints not found
         System.out.println("Pair not found");
     }
 
     // Problem 5.
+    //this method simply iterates through the array, and adds the absolute value
+    //of numbers that aren't already in the hashset. it then returns the hashset size
     public static int DistinctValues(int[] array){
+        //base case for empty or null array
         if (array.length == 0 || array == null){return 0;}
         HashSet<Integer> nums = new HashSet<Integer>();
+        //iterating through array
         for (int i : array){
+            //adding abs value of number only if they're not already added
             if (!nums.contains(Math.abs(i))){
                 nums.add(Math.abs(i));
             }
