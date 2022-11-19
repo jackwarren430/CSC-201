b8125ff6213a70309cbf4d2209b35aa931e04da5
Jack <jackwarren430@gmail.com>
2022-11-19 16:54:36 -0500

    worked on graphs

--- hw-assignments/3/JackWarren-Project3/GraphMatrix.java
+++ hw-assignments/3/JackWarren-Project3/GraphMatrix.java
@@ -7,9 +7,6 @@ public class GraphMatrix implements Graph{
     private int nodes;
     private int edges;
 
-    public GraphMatrix(){
-
-    }
 
     public void init(int n){
         matrix = new int[n][n];
@@ -27,44 +24,100 @@ public class GraphMatrix implements Graph{
     }
 
     public boolean hasEdge(int v, int w){
-        
+        if (v >= nodes || w >= nodes) {
+            System.out.println("ERROR: node does not exist");
+            return false;
+        } else if (matrix[v][w] > 0){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public void addEdge(int v, int w, int wgt){
-
+        if (v >= nodes || w >= nodes){
+            System.out.println("ERROR: node does not exist");
+            return;
+        }
+        matrix[v][w] = wgt;
+        edges++;
     }
 
     public void removeEdge(int v, int w){
-
+        if (v >= nodes || w >= nodes){
+            System.out.println("ERROR: node does not exist");
+            return;
+        }
+        matrix[v][w] = 0;
+        edges--;
     }
 
     public int getWeight(int v, int w){
-
+        if (v >= nodes || w >= nodes){
+            System.out.println("ERROR: node does not exist");
+            return -1;
+        }
+        return matrix[v][w];
     }
 
     public void setWeight(int v, int w, int wgt){
-
+        if (v >= nodes || w >= nodes){
+            System.out.println("Error: node does not exist");
+            return;
+        } else if (matrix[v][w] == 0){
+            System.out.println("ERROR: no edge exists here");
+            return;
+        }
+        matrix[v][w] = wgt;
     }
 
     public ArrayList<Integer> neighbors(int v){
-
+        if (v >= nodes){
+            System.out.println("ERROR: node does not exist");
+            return null;
+        }
+        ArrayList<Integer> toReturn = new ArrayList<Integer>();
+        for (int i = 0; i < nodes; i++){
+            int from = matrix[v][i];
+            int to = matrix[i][v];
+            if (from != to){
+                if (from > 0 && to > 0){
+                    toReturn.add(i);
+                } else if (from > 0){
+                    toReturn.add(i);
+                } else if (to > 0){
+                    toReturn.add(i);
+                }
+            }
+        }
+        return toReturn;
     }
 
     public void resetVisited(){
-
+        visited = new int[nodes][nodes];
     }
 
-    public ArrayList<Integer> BFS(){
-
+    public ArrayList<Integer> BFS(int v){
+        return null;
     }
 
     public boolean hasPath(int v, int w){
-
+        return true;
     }
 
     public ArrayList<Integer> topologicalSort(){
-
+        return null;
     }
 
+    public String toString(){
+        String toReturn = "";
+        for (int i = 0; i < nodes; i++){
+            for (int j = 0; j < nodes; j++){
+                toReturn += matrix[i][j] + " ";
+            }
+            toReturn += "\n";
+        }
+        return toReturn;
+    }
 
 }
--- hw-assignments/3/JackWarren-Project3/TestGraphMatrix.java
+++ hw-assignments/3/JackWarren-Project3/TestGraphMatrix.java
@@ -1,5 +1,16 @@
 public class TestGraphMatrix{
     public static void main(String[] args){
-        // For testing GraphMatrix implementaiton.
+        GraphMatrix matrix = new GraphMatrix();
+        matrix.init(5);
+        System.out.println(matrix);
+        matrix.addEdge(0, 1, 1);
+        matrix.addEdge(0, 2, 1);
+        matrix.addEdge(1, 3, 1);
+        matrix.addEdge(2, 4, 1);
+        matrix.addEdge(1, 4, 1);
+        System.out.println(matrix);
+        System.out.println(matrix.neighbors(0));
+        System.out.println(matrix.neighbors(4));
+        System.out.println(matrix.neighbors(1));
     }
 }
